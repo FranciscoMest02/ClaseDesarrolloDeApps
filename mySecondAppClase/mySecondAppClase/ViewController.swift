0c79b68c92ecc6b1831b48d5e08106ead76130eb
FranciscoMest02 <75438106+FranciscoMest02@users.noreply.github.com>
2022-09-30 08:42:21 -0500

    App de la clase 30 de sept 2022
    Aqui vimos uso de botones, labels y textfields. Hicimos alertas con un botón, cambiar el texto del label con el código y mostrar el texto del textfield en la alerta.

---
+++ mySecondAppClase/mySecondAppClase/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  mySecondAppClase
+//
+//  Created by Francisco Mestizo on 30/09/22.
+//  A01731549
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //IMPORTANTE. Un let es una variable que vamos a cambiar una sola vez en el codigo
+    //Pero una variable var, se puede cambiar n veces
+    
+    @IBAction func botonUnoClick(_ sender: Any) {
+        //Creamos la alerta para cuando se de click en el boton
+        let alert = UIAlertController(title: "Alerta!!", message: usuarioTextField.text, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "Cerrar", style: UIAlertAction.Style.cancel))
+        self.present(alert, animated: true, completion: nil)
+    }
+    @IBOutlet weak var tituloLabel: UILabel!
+    @IBOutlet weak var usuarioTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad() //Verifica que la aplicacion cargo
+        
+        tituloLabel.text = "Segunda Aplicacion"
+        tituloLabel.textColor = UIColor(red: 36/255, green: 80/255, blue: 155/255, alpha: 1.0)
+    }
+
+
+}
+
