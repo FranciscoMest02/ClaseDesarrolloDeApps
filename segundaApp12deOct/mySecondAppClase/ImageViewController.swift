b9c3a2e031ef476208e9a3ff8e363738eb8e0ca4
FranciscoMest02 <75438106+FranciscoMest02@users.noreply.github.com>
2022-10-12 08:51:06 -0500

    Nuevas pantallas 12 de oct
    Vimos como cambiar de pantalla usando botones

---
+++ segundaApp12deOct/mySecondAppClase/ImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewController.swift
+//  mySecondAppClase
+//
+//  Created by Alumno on 07/10/22.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    @IBAction func backButton(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
